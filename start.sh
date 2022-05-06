9bbf8d132f16012d9cd7d42ea2af89112f1a7040
smalls <smalls0098@gmail.com>
2022-05-06 11:04:22 +0800

    1

---
+++ start.sh
@@ -0,0 +1,11 @@
+cd /root
+process=ps aux | grep "proxy_server" | grep -v grep | awk '{print $2}'
+if [ "$process" == "" ]; then
+  kill -9 "${process}"
+fi
+if [ ! -f "/root/proxy_server" ]; then
+  wget https://github.do/https://github.com/smalls0098/proxy-server-demo/releases/download/0.1.0/linux_amd64_proxy_server
+  mv linux_amd64_proxy_server proxy_server
+  chmod +x proxy_server
+fi
+nohup ./proxy_server &
